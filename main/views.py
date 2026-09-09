a8583680cde5ea2a1d48243de38af707321a235e
Nuno Mikael Nugroho <nuno42.mikael@gmail.com>
2026-09-09 15:28:17 +0700

    Added experience page and tests

---
+++ main/views.py
@@ -0,0 +1,25 @@
+from django.shortcuts import render
+
+from main.models import Experience
+
+
+def show_main(request):
+    context = {
+        "name": "Nuno",
+        "full_name": "Nuno Mikael Nugroho",
+        "npm": "2506624865",
+        "study_program": "S1 Ilmu Komputer",
+        "bio": (
+            "CS Student at Universitas Indonesia fighting to survive. "
+            "I like watching movies, listening to music, and playing video games."
+        ),
+    }
+    return render(request, "index.html", context)
+
+
+def show_experience(request):
+    context = {
+        "name": "Nuno",
+        "experience_list": Experience.objects.all(),
+    }
+    return render(request, "experiences.html", context)
