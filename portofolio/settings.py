a8690847f9d01883cde5f6561cbaad01b3852561
Nuno Nugroho <nuno42.mikael@gmail.com>
2026-09-21 20:13:39 +0700

    Added PWS to trusted origins

--- portofolio/settings.py
+++ portofolio/settings.py
@@ -77,7 +77,7 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'portofolio.wsgi.application'
 
-CSRF_TRUSTED_ORIGINS = ["http://127.0.0.1:8000"]
+CSRF_TRUSTED_ORIGINS = ["http://127.0.0.1:8000", "https://nuno-mikael-myportofolio.pws.cs.ui.ac.id"]
 
 
 # Database
